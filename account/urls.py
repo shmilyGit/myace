008efbbc1a8403a4005d2434c5b2b5311ae15d63
shmilyjie <shmilyjie123@163.com>
2018-09-08 18:15:11 +0800

    1.修改登录页面,将X-admin的login.html更换为Ace的login.html 2.完成logout功能 3.学习基于类的视图

--- account/urls.py
+++ account/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import url
 from django.conf import settings
-from django.contrib.auth import views as auth_views
+from . import views as auth_views
 
 app_name='account'
 urlpatterns = [
 	#注意基本类的视图的写法 .as_view(template_name= TemplateName),不再是{"template_name":"account/logout.html"},后者是基于视图的写法
-	#django2.1已经使用基于类的视图代替了基于函数的视图
-	#url(r'^logout/$', auth_views.LogoutView.as_view, {"template_name":"account/logout.html"}, name="user_logout"),
+	#django2.1已经使用基于类的视图代替了基于函数的视图,不过真用基于视图的也可以
 
+	#url(r'^login/$', auth_views.login, {"template_name":"account/login.html"}, name="user_login"), #也可以使用,不推荐使用
 	url(r'^login/$', auth_views.LoginView.as_view(template_name='account/login.html'), name="user_login"),
-	url(r'^password-change/$', auth_views.PasswordChangeView.as_view(), name="password_change"),
+	url(r'^logout/$', auth_views.LogoutView.as_view(template_name='account/login.html'), name="user_logout"),
 ]
--- leace/urls.py
+++ leace/urls.py
@@ -17,14 +17,18 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url,include
 from django.views.generic import TemplateView
+from account import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^account/', include('account.urls', namespace='account')),
 
-	#登录之后跳转到该页,该页即首页
-    url(r'^index/', TemplateView.as_view(template_name="index.html"), name='index'),
+#登录之后跳转到该页,该页即首页
+    url(r'^index/', auth_views.ShowIndexPageView.as_view(), name='show_index'),
 
- 	#直接访问IP:port 后边不用加路径, 即http://192.168.5.105:8000就会跳转到login.html
-    url('', TemplateView.as_view(template_name="account/login.html"), name='loginIndex'),
+#直接访问IP:port 后边不用加路径, 即http://192.168.5.105:8000就会跳转到login.html
+#方法一,这样不会在html模板中使用{{form.password}}
+		#url('', TemplateView.as_view(template_name="account/login.html"), name='loginPage'),
+#方法二,这样可以在html模板中使用{{form.password}}
+		url('', auth_views.LoginView.as_view(template_name='account/login.html'), name="loginPage"),
 ]
